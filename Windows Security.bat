3f3c582aee4766794e51d490a68d52389fc80c0f
nathantle <116045835+nathantle@users.noreply.github.com>
2022-10-20 18:08:12 -0500

    Update Windows Security.bat

--- Windows Security.bat
+++ Windows Security.bat
@@ -18,6 +18,12 @@ echo net user username9 Cyb3rP@triot >> %UserProfile%\Desktop\_setpasswords.bat
 echo net user username10 Cyb3rP@triot >> %UserProfile%\Desktop\_setpasswords.bat
 echo rem >> %UserProfile%\Desktop\_setpassword.bat
 
+echo "This should only have valid administrators"
+net localgroup "Administrators"
+
+echo "This should only have valid users"
+net users
+
 echo echo on > %UserProfile%\Desktop\_delUsers.bat
 echo net user username11 /delete >> %UserProfile%\Destkop\_delUsers.bat
 echo net user username12 /delete >> %UserProfile%\Destkop\_delUsers.bat

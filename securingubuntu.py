c23024f06b30be60d602ab1da9336914ff25675b
nathantle <116045835+nathantle@users.noreply.github.com>
2023-09-03 19:40:54 -0500

    Update securingubuntu.py

--- securingubuntu.py
+++ securingubuntu.py
@@ -5,17 +5,20 @@ def run_command(command):
 
 def update_packages():
     print("Updating packages...")
+
     run_command("sudo apt update -y")
     run_command("sudo apt upgrade -y")
     run_command("sudo apt autoremove -y")
+
     print("Updated packages.")
 
 def configure_firewall():
     print("Configuring firewall...")
-    run_command("sudo ufw default deny incoming")
-    run_command("sudo ufw default allow outgoing")
-    run_command("sudo ufw allow ssh")
+
     run_command("sudo ufw enable")
+    run_command("sudo ufw allow ssh")
+    run_command("sudo ufw status verbose")
+
     print("Firewall configured")
 
 def disable_ssh_root_login():
@@ -43,11 +46,68 @@ def disable_ssh_root_login():
     #Restart SSH service
     run_command("sudo service ssh restart")
 
-    print("Root login disabled.")
+    print("SSH root login disabled.")
+    
+def enforce_ssh_key_authentication():
+    print("Enforcing SSH key authentication...")
+
+    #Disable password authentication in SSH server configuration
+    run_command("sudo sed -i s/#PasswordAuthentication yes/PasswordAuthentication no/ /etc/ssh/sshd.config")
+
+    #Restart SSH service
+    run_command("sudo systemctl restart ssh")
+
+    print("SSH key based authentication has been enforced.")
+
+def change_passwords(users, new_password):
+    print("Changing passwords...")
+
+    for user in users:
+        #Change the password using passwd
+        passwd_process = subprocess.Popen(['sudo', 'passwd', user], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        passwd_process.communicate(input=f'{new_password}\n{new_password}\n'.encode())
+
+        # Check the exit code to determine if the password change was successful
+        if passwd_process.returncode == 0:
+            print(f"Password changed successfully for user '{user}'")
+        else:
+            print(f"Failed to change password for user '{user}'")
+
+def remove_bad_services():
+    print("Removing bad services...")
+
+    #Removing telnet and ftp
+    run_command("sudo apt remove -purge -y telnet ftp")
+
+def set_log_file_permissions():
+    print("Setting appropiate permissions on the log file")
+
+    #Removing rwx for o and g
+    run_command("sudo chmod -R g-rwx, o-rwx /var/log")
+
+def lock_out_root():
+    print("Locking out root user...")
+
+    run_command("sudo passwd -l root")
+
+    print("Root user locked out")
+
+def disable_guest():
+    print("Disabling guest account...")
+
+    run_command()
+
+# DO NOT FORGET TO SET THESE VARIBLES
+# Configuring user_list and new_password for change password function
+user_list = ['user1', 'user2', 'user3']
+new_password = "new_password"
 
 def main():
-    disable_ssh_root_login()
-    print("System secured.")
-
-if __name__ == "__main__":
-    main()
+    # update_packages()
+    #disable_ssh_root_login()
+    # change_passwords(user_list, new_password)
+    # enforce_ssh_key_authentication()
+    remove_bad_services()
+    set_log_file_permissions()
+    lock_out_root()
+    # disable_guest()

46d40ae0b2c60888279428436e166fd51d9f669d
nathantle <nathantle.pub@gmail.com>
2023-10-09 17:43:50 -0500

    Update to basics.py and users.py

---
+++ ubuntu/users.py
@@ -0,0 +1,119 @@
+import subprocess
+
+def run_command(command):
+    subprocess.run(command, shell=True, check=True)
+
+def users():
+    done = False
+
+    authadmns = []
+    authusrs = []
+
+    while(done != True):
+        authadmn = input("Enter an authorized admin: ")
+        authadmns.append(authadmn)
+        bo = input("Done?(y/n)").lower()
+        if(bo == "y"):
+            done = True
+        else:
+            done = False
+    done = False
+    while(done != True):
+        authusr = input("Enter an authorized user: ")
+        authusrs.append(authusr)
+        bo = input("Done?(y/n)").lower()
+        if(bo == "y"):
+            done = True
+        else:
+            done = False
+    
+
+    process = subprocess.Popen("cut -d: -f1 /etc/passwd", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    if process.returncode == 0:
+        ulist = output.decode().splitlines()
+        defaultuser = input("Enter default root user: ").lower()
+        defaultusrs = ["root", "daemon", "bin", "sys", "sync", "games", "man", "lp", "mail", "news", "uucp", "proxy", "www-data", "backup", "list", "irc", "gnats", "nobody", "systemd.network", "systemd-resolve", "messagebus", "systemd-timesync", "syslog", "_apt", "tss", "uuidd", "avahi-autoipd", "usbmux", "dnsmasq", "kernoops", "avahi", "cups-pk-helper", "rtkit", "whoopsie", "sssd", "speech-dispatcher", "nm-openvpn", "saned", "colord", "geoclue", "pulse", "gnome-initial-setup", "hplip", "gdm", "_rpc", "statd", "sshd", "systemd-network", "systemd-oom", "tcpdump"]
+        ulist.remove(defaultuser)
+        usrlist = [e for e in ulist if e not in defaultusrs]
+    else:
+        print(f"Error: {error.decode()}")
+        return []
+    
+    command = "grep '^sudo:.*$' /etc/group | cut -d: -f4"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, _ = process.communicate()
+    admins = output.decode().strip().split(",")
+    admins.remove(defaultuser)
+
+    for user in usrlist:
+        if user not in authusrs + authadmns:
+            run_command(f"sudo deluser {user}")
+        newpass = "Cyb3rP@triot24!"
+        passwd_process = subprocess.Popen(['sudo', 'passwd', user], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        passwd_process.communicate(input=f'{newpass}\n{newpass}\n'.encode())
+
+        # Check the exit code to determine if the password change was successful
+        if passwd_process.returncode == 0:
+            print(f"Password changed successfully for user '{user}'")
+        else:
+            print(f"Failed to change password for user '{user}'")
+        
+        #Setting correct user permissions
+        process = subprocess.Popen(f"sudo groups {user}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, _ = process.communicate()
+        groups = output.decode().strip().split()
+
+        if "sudo" in groups:
+            sudoer = True
+        else:
+            sudoer = False
+
+        if sudoer and user not in authadmns:
+            run_command(f"sudo deluser {user} sudo")
+        elif not sudoer and user in authadmns:
+            run_command(f"sudo adduser {user} sudo")
+
+    doneaddingusers = False
+
+    while not doneaddingusers:
+        wanttoaddusers = input("Add a user(y/n)").lower()
+        if wanttoaddusers == "y":
+            usertoadd = input("Enter name of user to add: ")
+            run_command(f"sudo adduser {usertoadd}")
+            moreuserstoadd = input("Any more?(y/n)")
+            if moreuserstoadd == "y":
+                doneaddingusers = False
+            else:
+                doneaddingusers = True
+        else:
+            doneaddingusers = True
+
+    run_command("sudo passwd -l root")
+
+def groups():
+    doneaddinggroups = False
+
+    while not doneaddinggroups:
+        grouptoadd = input("Enter the name of the group to add:")
+        run_command(f"sudo addgroup {grouptoadd}")
+
+        doneaddinguserstogroup = False
+
+        while(not doneaddinguserstogroup):
+            usertoadd = input("Enter the name of the user to add to the group:")
+            run_command(f"sudo useradd {usertoadd} {grouptoadd}")
+            finishedaddinguserstogroup = input("Any more users?(y/n)").lower()
+        if finishedaddinguserstogroup == "y":
+            doneaddinguserstogroup = True
+        else:
+            doneaddinguserstogroup = False
+        finishedaddinggroups = input("Add another group?(y/n)").lower()
+        if finishedaddinggroups == "y":
+            doneaddinggroups == True
+        else:
+            doneaddinggroups == False
+
+def all():
+    users()
+    groups()

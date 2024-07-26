3efcd5f71c257217d497ba0eb2646ce3fc07b716
nathantle <116045835+nathantle@users.noreply.github.com>
2024-07-26 13:29:57 -0500

    update

--- linux/secure.py
+++ linux/secure.py
@@ -3,12 +3,29 @@ import subprocess
 # Manual Tasks - Configure Software and Updates
 print("")
 
-# Updates
-try:
-    subprocess.run(["sudo", "apt", "update"])
-    subprocess.run(["sudo", "apt", "full_upgrade"])
-    subprocess.run(["sudo", "apt", "autoremove"])
-except Exception:
-    print("Error updating packages")
 
-# 
+proceed = input("Press enter to proceed to updates(q to stop)")
+if proceed != "q": 
+# Updates
+    try:
+        subprocess.run(["sudo", "apt", "update"])
+        subprocess.run(["sudo", "apt", "full_upgrade"])
+        subprocess.run(["sudo", "apt", "autoremove"])
+    except Exception:
+        print("Error updating packages")
+
+# Configure Firewall
+proceed = input("Press enter to proceed to configuring firewall(q to stop)")
+if proceed != "q": 
+    try:
+        subprocess.run(["sudo", "ufw", "enable"])
+    except Exception:
+        print("Error configuring firewall")
+
+# Configure ssh
+proceed = input("Press enter to proceed to configuring ssh(q to stop)")
+if proceed != "q": 
+    try:
+        subprocess.run(["sudo", "systemctl", "enable","ssh"])
+    except Exception:
+        print("Error configuring ssh")
